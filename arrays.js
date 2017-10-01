00f9b7efc870e69b929be44082334579796f0a90
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-01 12:20:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return [candyString, ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return [...chocolateBars, candyString];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  return chocolateBars.shift();
+}
+
+function removeElementFromBeginningOfArray(chocolateBars) {
+  return chocolateBars.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
+  return chocolateBars.pop();
+}
+
+function removeElementFromEndOfArray(chocolateBars) {
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
